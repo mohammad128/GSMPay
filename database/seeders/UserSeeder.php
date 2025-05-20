c5d04559b2b8766ac0a8fb5c2bbb45e8161fbbfe
MohammadJ <javadi.mohammad128@gmail.com>
2025-05-20 12:40:34 +0330

    add user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'mobile' => '09363680395',
+            'password' => Hash::make('password'),
+        ]);
+
+        User::factory()->create([
+            'mobile' => '09020902272',
+            'password' => Hash::make('password'),
+        ]);
+    }
+}
